3d13e8f2ca9543d3912020c11a819a97caaa71df
ja54312 <ija54312@gmail.com>
2023-01-31 17:07:07 -0600

    product page

--- client/src/pages/Product/Product.jsx
+++ client/src/pages/Product/Product.jsx
@@ -1,10 +1,80 @@
-import React from 'react'
-import './product.css'
+import React, { useState } from "react";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import BalanceIcon from "@mui/icons-material/Balance";
+import "./product.css";
 
 const Product = () => {
+  const [selectedImg, setSelectedImg] = useState(0);
+  const [quantity, setQuantity] = useState(1);
+  const images = [
+    "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  ];
   return (
-    <div>Product</div>
-  )
-}
+    <div className="product">
+      <div className="left">
+        <div className="images">
+          <img
+            src={images[0]}
+            alt="imagen persona"
+            onClick={(e) => setSelectedImg(0)}
+          />
+          <img
+            src={images[1]}
+            alt="imagen persona"
+            onClick={(e) => setSelectedImg(1)}
+          />
+        </div>
+        <div className="mainImg">
+          <img src={images[selectedImg]} alt="imagen persona" />
+        </div>
+      </div>
+      <div className="right">
+        <h2>Title</h2>
+        <span className="price">$199</span>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
+          blanditiis eveniet ipsa nostrum animi! Consequatur, maxime aspernatur
+          voluptatum quisquam laborum necessitatibus voluptates magni sapiente,
+          facilis ad fugiat nihil eum modi?
+        </p>
+        <div className="quantity">
+          <button
+            onClick={() => setQuantity((prev) => (prev === 1 ? 1 : prev - 1))}
+          >
+            -
+          </button>
+          {quantity}
+          <button onClick={() => setQuantity((prev) => prev + 1)}>+</button>
+        </div>
+        <button className="add">
+          <AddShoppingCartIcon /> ADD TO CART
+        </button>
+        <div className="links">
+          <div className="item">
+            <FavoriteBorderIcon /> ADD TO WISH LIST
+          </div>
+          <div className="item">
+            <BalanceIcon /> ADD TO COMPARE
+          </div>
+        </div>
+        <div className="info">
+          <span>Vendor: Polo</span>
+          <span>Product Type: T-Shirt</span>
+          <span>Tag: T-Shirt, Women, Top</span>
+        </div>
+        <hr />
+        <div className="info">
+          <span>DESCRIPTION</span>
+          <hr />
+          <span>ADDITIONAL INFORMATION</span>
+          <hr />
+          <span>FAQ</span>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Product
+export default Product;
