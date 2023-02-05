73c9e4de10fdd2407fb06ac681e903eb111072ff
ja54312 <ija54312@gmail.com>
2023-02-05 13:42:01 -0600

    conection

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,24 +1,38 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-import './Card.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Card.css";
 
-const Card = ({item}) => {
+const Card = ({ item }) => {
   return (
-    <Link className='link' to={`/product/${item.id}`}>
-    <div className='card'>
+    <Link className="link" to={`/product/${item.id}`}>
+      <div className="card">
         <div className="image">
-        {item.isNew && <span>New Season</span>}
-            <img src={item.img} alt="imagen principal" className="mainImg" />
-            <img src={item.img2} alt="imagen secundaria" className="secondImg" />
+          {item?.attributes.isNew && <span>New Season</span>}
+          <img
+            src={
+              process.env.REACT_APP_UPLOAD_URL +
+              item.attributes?.img?.data?.attributes?.url
+            }
+            alt="one"
+            className="mainImg"
+          />
+          <img
+            src={
+              process.env.REACT_APP_UPLOAD_URL +
+              item.attributes?.img2?.data?.attributes?.url
+            }
+            alt="two"
+            className="secondImg"
+          />
         </div>
-        <h2>{item.title}</h2>
+        <h2>{item?.attributes.title}</h2>
         <div className="prices">
-            <h3>${item.oldPrice}</h3>
-            <h3>${item.price}</h3>
+          <h3>${item.oldPrice || item?.attributes.price + 20}</h3>
+          <h3>${item?.attributes.price}</h3>
         </div>
-    </div>
+      </div>
     </Link>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
--- client/src/components/FeaturedProducts/FeaturedProducts.jsx
+++ client/src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -1,47 +1,16 @@
 import React from 'react'
+import useFetch from '../../hooks/useFetch'
 import Card from '../Card/Card'
 import './FeaturedProducts.css'
 
 const FeaturedProducts = ({type}) => {
 
-    const data = [
-        {
-            id:1,
-            img:'https://images.pexels.com/photos/1308885/pexels-photo-1308885.jpeg',
-            img2:'https://images.pexels.com/photos/899740/pexels-photo-899740.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-            title:"Vestido Asiatico",
-            isNew:true,
-            oldPrice:19,
-            price:12
-        },
-        {
-            id:2,
-            img:'https://images.pexels.com/photos/691234/pexels-photo-691234.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-            img2:'https://images.pexels.com/photos/2496451/pexels-photo-2496451.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-            title:"Vestido de Seda",
-            isNew:true,
-            oldPrice:23,
-            price:18
-        },
-        {
-            id:3,
-            img:'https://images.pexels.com/photos/1149837/pexels-photo-1149837.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-            img2:'https://images.pexels.com/photos/1435438/pexels-photo-1435438.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-            title:"Top Femenino",
-            isNew:false,
-            oldPrice:10,
-            price:8
-        },
-        {
-            id:4,
-            img:'https://images.pexels.com/photos/2205415/pexels-photo-2205415.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-            img2:'https://images.pexels.com/photos/2496427/pexels-photo-2496427.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-            title:"Ropa Juvenil",
-            isNew:false,
-            oldPrice:10,
-            price:7
-        }
-    ]
+    const { data, loading, error } = useFetch(
+        `/products?populate=*&[filters][type][$eq]=${type}`
+      );
+
+      console.log(data)
+
   return (
     <div className='featuredProducts'>
         <div className="top">
@@ -49,11 +18,11 @@ const FeaturedProducts = ({type}) => {
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa alias sit tempore voluptates veritatis illum natus minima nobis ut non, modi sequi dolore corrupti incidunt perspiciatis autem soluta aperiam suscipit!</p>
         </div>
         <div className="bottom">
-            {
-                data.map(item=>(
-                    <Card item={item} key={item.id}/>
-                ))
-            }
+        {error
+          ? "Something went wrong!"
+          : loading
+          ? "loading"
+          : data?.map((item) => <Card item={item} key={item.id} />)}
         </div>
     </div>
   )
